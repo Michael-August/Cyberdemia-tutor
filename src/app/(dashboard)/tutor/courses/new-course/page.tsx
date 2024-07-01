e44c448b269d98f73bb4e7cea7561045bd93d6a8
Michael-August <michaelfrancis135@gmail.com>
2024-07-01 15:10:25 +0100

    dashboard page completed (TUTOR)

--- src/app/(dashboard)/tutor/courses/new-course/page.tsx
+++ src/app/(dashboard)/tutor/courses/new-course/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 const page = () => {
-//   const { dispatch } = useLayoutContext();
+  //   const { dispatch } = useLayoutContext();
 
-//   useEffect(() => {
-//     dispatch({ type: 'SET_NAVBAR', navbarType: 'dashboardNavbar' });
-//     dispatch({ type: 'SET_SIDEBAR', sidebarType: 'defaultSidebar' });
-//   }, [dispatch]);
+  //   useEffect(() => {
+  //     dispatch({ type: 'SET_NAVBAR', navbarType: 'dashboardNavbar' });
+  //     dispatch({ type: 'SET_SIDEBAR', sidebarType: 'defaultSidebar' });
+  //   }, [dispatch]);
 
   return <div>New Course</div>;
 };
